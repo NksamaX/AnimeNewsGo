c5b07e3b92f2a0afd8a7a7fbd5fff3d97be671c2
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-30 12:17:57 +0530

    Update news.go

--- news/news.go
+++ news/news.go
@@ -8,7 +8,6 @@ import (
 
 // To get The news from AnimeNewsNetwork
 // fmt.Println(Get_News())
-
 func Get_News() string {
 	var url string = "https://animenewsnetwork.com"
 	res, err := soup.Get(url)
