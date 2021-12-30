9ad231eca0a4c3000006935f03e64ef33462d0b2
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-30 11:44:00 +0530

    Create news.go

---
+++ news/news.go
@@ -0,0 +1,24 @@
+package news
+
+import (
+	"fmt"
+
+	"github.com/anaskhan96/soup"
+)
+
+func Get_News() string {
+	var url string = "https://animenewsnetwork.com"
+	res, err := soup.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	doc := soup.HTMLParse(res)
+	news := doc.FindStrict("div", "class", "herald box news")
+	data := news.Find("div", "class", "wrap")
+	title := data.Find("h3").Find("a").FullText()
+	newsx := data.Find("div", "class", "preview").Find("span", "class", "full")
+
+	return fmt.Sprintf("%v\n\n%v", title, newsx.FullText())
+
+}
