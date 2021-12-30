e49ea54ab65c7fabedb139d5b24d3d2f3c38b324
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-30 11:45:42 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module animenews
+
+go 1.16
+
+require github.com/anaskhan96/soup v1.2.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"animenews/news"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(news.Get_News())
+}
