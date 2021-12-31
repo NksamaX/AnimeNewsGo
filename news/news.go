6791e71c1c335fb2287dd7548b85203f528e6d02
Yαƚσ <75163978+Nksama@users.noreply.github.com>
2021-12-31 09:11:46 +0530

    Update news.go

--- news/news.go
+++ news/news.go
@@ -8,7 +8,7 @@ import (
 
 // To get The news from AnimeNewsNetwork
 // fmt.Println(Get_News())
-func Get_News() string {
+func Get_News() map[string]string {
 	var url string = "https://animenewsnetwork.com"
 	res, err := soup.Get(url)
 	if err != nil {
@@ -21,6 +21,11 @@ func Get_News() string {
 	title := data.Find("h3").Find("a").FullText()
 	newsx := data.Find("div", "class", "preview").Find("span", "class", "full")
 
-	return fmt.Sprintf("%v\n\n%v", title, newsx.FullText())
+// 	return fmt.Sprintf("%v\n\n%v", title, newsx.FullText())
+	result = make(map[string]string)
+	result["title"] = title
+	result["news"] = newsx
+	
+	return result
 
 }
